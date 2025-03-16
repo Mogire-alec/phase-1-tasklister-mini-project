b7f400a681d74fff748a1c638985bb6c7ed350d9
Alex <alexandermogire009@gmail.com>
2025-03-16 12:51:01 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ddocument.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('task-form'); 
+  const taskList = document.getElementById('task-list'); 
+  
+  form.addEventListener('submit', function (event) {
+      
+      event.preventDefault();
+
+     
+      const taskInput = document.getElementById('task-input'); 
+      const taskText = taskInput.value.trim();
+
+      /
+      if (taskText !== '') {
+         
+          const newTask = document.createElement('div');
+          newTask.textContent = taskText;
+
+         
+          taskList.appendChild(newTask);
+          taskInput.value = '';
+      }
+  });
 });
